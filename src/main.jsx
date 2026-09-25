030de8f40cca9395be90bfad5c8d30dddcdf020b
Lukas Radzevičius <lukas.radzevicius@nrd.lt>
2026-09-25 13:35:39 +0300

    added instagram link

--- src/main.jsx
+++ src/main.jsx
@@ -200,7 +200,7 @@ const socialLinks = [
   },
   {
     name: "Instagram",
-    href: "https://google.com",
+    href: "https://www.instagram.com/chorassandora/",
     icon: (
       <g fill="none" stroke="currentColor" strokeWidth="2">
         <rect x="3" y="3" width="18" height="18" rx="5" />
