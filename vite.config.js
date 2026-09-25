7b190ccb062156ea1998f4cf24a3ac82d2d941f0
Lukas Radzevičius <100800444+Luk-Rad-NRDdigital@users.noreply.github.com>
2026-09-25 11:33:51 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({command}) => ({
   // GitHub project sites live under the repository name; local development stays at /.
-  base: command === 'build' ? '/ChoirSandora/' : '/',
+  base: command = '/',
 }));
